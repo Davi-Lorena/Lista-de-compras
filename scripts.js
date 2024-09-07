d6b241a89ae5c999d382054b6fcdaf7c7ec8017d
Davi Lorena <davilorena2016@gmail.com>
2024-09-07 14:23:53 -0300

    adding remove item

--- scripts.js
+++ scripts.js
@@ -12,35 +12,53 @@ const addNewItem = () => {
 
     const inputIsValid = validateInput()
     if(!inputIsValid) {
-        return(alert("error"))
+        return(alert("Digite um item!"))
     }
  
     const li = document.createElement("li")
     li.classList.add("item")
+    li.style.marginBottom = ` .75rem `
 
-    li.innerHTML = `
-    <button>
-    <img src="./assets/icons/Frame-3.svg" alt="">
-    </button>
-    `  
-    
 const div = document.createElement("div")
 const checkbox = document.createElement("input")
 checkbox.setAttribute("type", "checkbox")
     const span = document.createElement("span")
     span.textContent = input.value 
+const del = document.createElement("button")
+del.innerHTML = `<img src="./assets/icons/Frame-3.svg" alt="">` 
 
-    div.append(checkbox, span)
-li.prepend(div)
+div.append(checkbox, span)
+li.prepend(div, del)
     ul.append(li) 
 
     input.value = ""
+
+   del.addEventListener("click", () => { 
+ const circle = document.createElement("small")
+ circle.innerHTML = `<img src="./assets/icons/warning-circle-filled.svg" alt="">`
+div.prepend(circle)
+ checkbox.remove("input") 
+ del.remove("button")
+
+const btn = document.createElement("button")
+btn.innerHTML = `<img src="./assets/icons/delete-small.svg" alt="">`
+
+li.append(btn)
+     span.textContent = "O item foi removido da lista"
+     li.classList.replace("item", "alert")
+
+
+     btn.addEventListener("click", () => {
+        li.classList.replace("alert", "delet")
+     })
+
+ }) 
+
 }
-
-
 
 button.addEventListener("click", () => addNewItem()) 
 
 form.onsubmit = (event) => {
     event.preventDefault()
+
 }
